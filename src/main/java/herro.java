856c198e8041780a4d695ce9ae535cc3711825b3
ASUS_a.vas <a.vasenina@setplex.com>
2019-07-22 20:36:00 +0300

    qwe

---
+++ src/main/java/herro.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class herro {
+    public static final String  URL ="http://the-internet.herokuapp.com/add_remove_elements/";
+
+    @Test
+    public void addRemovalElement(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/webdrivers/chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.get(URL);
+        WebElement addElement = driver.findElement(By.xpath("//*[text()='Add Element']"));
+        addElement.click();
+        List<WebElement> deleteElement = driver.findElements(By.xpath("//*[text()='Delete']"));
+        assertEquals(deleteElement.size(),1);
+        addElement.click();
+        deleteElement = driver.findElements(By.xpath("//*[text()='Delete']"));
+        assertEquals(deleteElement.size(),2);
+        deleteElement.get(1).click();
+        deleteElement = driver.findElements(By.xpath("//*[text()='Delete']"));
+
+        driver.manage().window().maximize();
+        driver.quit();
+
+    }
+}
